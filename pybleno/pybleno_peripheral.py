9294f423c60fd2c16d86059d2ebb900d677e8cd2
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2020-03-10 20:56:10 +0900

    REAME.md

--- pybleno/pybleno_peripheral.py
+++ pybleno/pybleno_peripheral.py
@@ -1,16 +1,17 @@
-from pybleno import *
+#! /usr/bin/env python3
 
-bleno = Bleno()
+from pybleno import Bleno, Characteristic, BlenoPrimaryService
+import time
 
-APPROACH_SERVICE_UUID = '13A28130-8883-49A8-8BDB-42BC1A7107F4'
-APPROACH_CHARACTERISTIC_UUID = 'A2935077-201F-44EB-82E8-10CC02AD8CE1'
+FOO_SERVICE_UUID = '13A28130-8883-49A8-8BDB-42BC1A7107F4'
+FOO_CHARACTERISTIC_UUID = 'A2935077-201F-44EB-82E8-10CC02AD8CE1'
 
 
-class ApproachCharacteristic(Characteristic):
+class FooCharacteristic(Characteristic):
 
     def __init__(self):
         Characteristic.__init__(self, {
-            'uuid': APPROACH_CHARACTERISTIC_UUID,
+            'uuid': FOO_CHARACTERISTIC_UUID,
             'properties': ['read', 'notify'],
             'value': None
         })
@@ -23,12 +24,12 @@ class ApproachCharacteristic(Characteristic):
         callback(Characteristic.RESULT_SUCCESS, self._value)
 
     def onSubscribe(self, maxValueSize, updateValueCallback):
-        print('ApproachCharacteristic - onSubscribe')
+        print('FooCharacteristic - onSubscribe')
 
         self._updateValueCallback = updateValueCallback
 
     def onUnsubscribe(self):
-        print('ApproachCharacteristic - onUnsubscribe')
+        print('FooCharacteristic - onUnsubscribe')
 
         self._updateValueCallback = None
 
@@ -37,52 +38,48 @@ def onStateChange(state):
     print('on -> stateChange: ' + state)
 
     if (state == 'poweredOn'):
-        bleno.startAdvertising('Approach', [APPROACH_SERVICE_UUID])
+        bleno.startAdvertising('Foo', [FOO_SERVICE_UUID])
     else:
         bleno.stopAdvertising()
 
 
-bleno.on('stateChange', onStateChange)
-
-approachCharacteristic = ApproachCharacteristic()
-
-
 def onAdvertisingStart(error):
-    print('on -> advertisingStart: ' + ('error ' + error if error else 'success'))
+    print('on -> advertisingStart: ' + (
+        'error ' + error if error else 'success'))
 
     if not error:
         bleno.setServices([
             BlenoPrimaryService({
-                'uuid': APPROACH_SERVICE_UUID,
+                'uuid': FOO_SERVICE_UUID,
                 'characteristics': [
-                    approachCharacteristic
+                    fooCharacteristic
                 ]
             })
         ])
 
 
-bleno.on('advertisingStart', onAdvertisingStart)
-
-bleno.start()
-
-
-import time
-
 counter = 0
+
 
 def task():
     global counter
     counter += 1
-    approachCharacteristic._value = counter
-    if approachCharacteristic._updateValueCallback:
+    fooCharacteristic._value = counter
+    if fooCharacteristic._updateValueCallback:
 
-        print('Sending notification with value : ' + str(approachCharacteristic._value))
+        print('Sending notification with value : ' + str(
+            fooCharacteristic._value))
 
-        notificationBytes = str(approachCharacteristic._value).encode()
-        approachCharacteristic._updateValueCallback(notificationBytes)
+        notificationBytes = str(fooCharacteristic._value).encode()
+        fooCharacteristic._updateValueCallback(notificationBytes)
 
+
+bleno = Bleno()
+fooCharacteristic = FooCharacteristic()
+bleno.on('stateChange', onStateChange)
+bleno.on('advertisingStart', onAdvertisingStart)
+bleno.start()
 
 while True:
-    print('aaa')
     task()
     time.sleep(1)
