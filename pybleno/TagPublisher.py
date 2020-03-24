97efb532538309a886d02ce8e3cf0c381b20e3ab
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2020-03-25 07:10:31 +0900

    update

--- pybleno/TagPublisher.py
+++ pybleno/TagPublisher.py
@@ -28,7 +28,8 @@ class TagPublisher(BlePeripheral):
         self._ms_data = (self.TAGID_PREFIX + self._tagid).encode('utf-8')
         self._log.debug('_ms_data=%a', self._ms_data)
 
-        super().__init__(self._myname, [self._svc1], self._ms_data,
+        # super().__init__(self._myname, [self._svc1], self._ms_data,
+        super().__init__(self._myname, [], self._ms_data,
                          debug=self._dbg)
 
 
--- serial/get-tagid.py
+++ serial/get-tagid.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import serial
-import time
 from MyLogger import get_logger
 import click
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
@@ -12,13 +11,15 @@ class App:
 
     _log = get_logger(__name__, False)
 
-    def __init__(self, dev_name, speed, debug=False):
+    def __init__(self, dev_name_prefix, speed, repeat=False, debug=False):
         self._dbg = debug
         self._log = get_logger(__class__.__name__, self._dbg)
-        self._log.debug('dev_name=%s, speed=%s', dev_name, speed)
+        self._log.debug('dev_name_prefix=%s, speed=%s, repeat=%s',
+                        dev_name_prefix, speed, repeat)
 
-        self._dev_name = dev_name
+        self._dev_name_prefix = dev_name_prefix
         self._speed = speed
+        self._repeat = repeat
 
         self._ser = None
 
@@ -26,27 +27,28 @@ class App:
         self._log.debug('')
 
         while True:
-            tag_addr = self.get_tagaddr()
+            tag_addr = self.get_tagaddr(self._repeat)
             if tag_addr is not None:
                 break
 
         self._log.info('tag_addr=%s.', tag_addr)
 
-    def get_tagaddr(self):
-        self._log.debug('')
+    def get_tagaddr(self, repeat):
+        self._log.debug('repeat=%s', self._repeat)
 
-        if self.openSerial('/dev/ttyUSB', 115200, 1.0) is None:
+        dev = self.openSerial(self._dev_name_prefix, 115200, 1.0)
+        if dev is None:
             self._log.error('failed to open serial')
             return
 
+        tag_addr = None
         while True:
             try:
                 li = self._ser.readline()
             except serial.serialutil.SerialException as e:
                 self._log.warning('%s:%s', type(e).__name__, e)
                 li = b''
-                return None
-                
+
             if len(li) == 0:
                 continue
 
@@ -56,16 +58,19 @@ class App:
                 li = str(li)
 
             li = li.replace('\r\n', '')
-            self._log.debug("%s", li)
+            self._log.debug("%s>%s", dev, li)
 
             if li.startswith(self.STR_MY_ADDR):
                 tag_addr = li[len(self.STR_MY_ADDR):]
                 self._log.debug('* tag_addr=%s.', tag_addr)
 
-                self.closeSerial()
-                return tag_addr
+                if repeat:
+                    continue
+
+                break
 
         self.closeSerial()
+        return tag_addr
 
     def end(self):
         self._log.debug('')
@@ -75,6 +80,8 @@ class App:
     def openSerial(self, dev_prefix, speed, timeout=1.0):
         self._log.debug('dev_prefix=%s, spped=%d, timeout=%s',
                         dev_prefix, speed, timeout)
+
+        dev = None
 
         if self._ser is not None:
             self._log.warning('already opend .. close')
@@ -88,9 +95,10 @@ class App:
                 break
             except Exception as e:
                 self._log.error('%s, %s', type(e).__name__, e)
+                dev = None
                 continue
 
-        return self._ser
+        return dev
 
     def closeSerial(self):
         self._log.debug('')
@@ -102,18 +110,21 @@ class App:
 @click.command(context_settings=CONTEXT_SETTINGS, help='''
 Serial test
 ''')
-@click.option('--dev_name', '-d', 'dev_name', type=str,
-              default='/dev/ttyUSB0',
+@click.option('--dev_name_prefix', '-d', 'dev_name_prefix', type=str,
+              default='/dev/ttyUSB',
               help='serial device name')
 @click.option('--speed', '-s', 'speed', type=int, default=9600,
               help='serial speed')
+@click.option('--repeat', '-r', 'repeat', is_flag=True, default=False,
+              help='repeat flag')
 @click.option('--debug', '-d', 'debug', is_flag=True, default=False,
               help='debug option')
-def main(dev_name, speed, debug):
+def main(dev_name_prefix, speed, repeat, debug):
     log = get_logger(__name__, debug)
-    log.debug('dev_name=%s, speed=%s', dev_name, speed)
+    log.debug('dev_name_prefix=%s, speed=%s, repeat=%s',
+              dev_name_prefix, speed, repeat)
 
-    app = App(dev_name, speed, debug=debug)
+    app = App(dev_name_prefix, speed, repeat, debug=debug)
     try:
         app.main()
     finally:
